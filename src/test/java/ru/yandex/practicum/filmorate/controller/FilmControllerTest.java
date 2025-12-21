56560ae9685f7ae15c6ce85cf16d02194e0f0289
saleevakate <saleevakate@yandex.ru>
2025-12-21 18:25:52 +0500

    Исправлены тесты

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -14,7 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FilmControllerTest {
 
     private FilmController filmController = new FilmController();
-    ;
     Film film = new Film();
 
     @BeforeEach
