535810ec00cf22196ac71496025d1f6ad5b9241d
saleevakate <saleevakate@yandex.ru>
2026-02-14 21:00:54 +0500

    фз 11

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -20,6 +20,6 @@ public class User {
     private String name;
 
     @NotNull(message = "Дата рождения не может быть null")
-    @Past(message = "Дата рождения не может быть в будущем")
+    @Past(message = "Дата рождения не  может быть в будущем")
     private LocalDate birthday;
 }
